3919cdf19c445fe67577b78995f52ef1a7558191
Dikshant Rajput <dikshantraj2001@gamil.com>
2022-02-11 00:58:32 +0530

    port

--- server/index.js
+++ server/index.js
@@ -24,10 +24,10 @@ app.get('/api/greeting', (req, res) => {
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
-// const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001;
 
 const server = app.listen(15635, () =>
-  console.log('Express server is running on ' + 15635)
+  console.log('Express server is running on ' + PORT)
 );
 
 Gun({ file: 'db', web: server });
