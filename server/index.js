80f8ee4a91c2658d47bba4615d3080c6c29f9718
Dikshant Rajput <dikshantraj2001@gamil.com>
2022-02-11 00:09:54 +0530

    add

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 const Gun = require('gun/gun')
+const cors = require('cors');
 
 const app = express();
 
