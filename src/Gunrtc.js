ab43d9231472b3b1b947e6707ed61ce0487b3073
Dikshant Rajput <dikshantraj2001@gamil.com>
2022-02-11 18:59:59 +0530

    add

--- src/Gunrtc.js
+++ src/Gunrtc.js
@@ -11,15 +11,29 @@ function Gunrtc() {
   const gun = useRef(null)
   
   useEffect(()=>{
-    gun.current = Gun('http://test-appsasdas.herokuapp.com/gun')
-    setUsers(gun.current.get('users'))
-    gun.current.get('users').get('teams').map().once((item,id)=>{
+    gun.current = Gun('http://test-appsasdas.herokuapp.coms/gun')
+    setUsers(gun.current.get('teams'))
+    gun.current.get('teams').map().once((item,id)=>{
       setLocalUsers((prev)=>[...prev,{id,name:item?.n,points:item?.p}])
     },{wait:0})
   },[])
-
+  
   const clickHandler = ()=>{
-    users.get('teams').set({n:name,p:points})
+    users.set({n:name,p:points})
+  }
+  
+  const incrementPoints = (id)=>{
+    gun.current.get('teams').get(id).once((i)=>{
+      let points = parseFloat(i.p)
+      gun.current.get('teams').get(id).put({p:points+1})
+      
+      const filter = localUsers.filter((item)=>item.id != id)
+
+      const item = localUsers.find((item)=>item.id == id)
+      item.points++
+      setLocalUsers([...filter,item])
+    })
+    
   }
   
   return (
@@ -29,9 +43,19 @@ function Gunrtc() {
       <button onClick={clickHandler} >Add</button>
       {
         localUsers.sort((a,b)=>a.points - b.points).map((i)=>{
-          return <div key={i?.id}>{i?.name} - {i?.points} </div>
+          return <div key={i?.id}>{i?.name} - {i?.points} <button onClick={()=>incrementPoints(i?.id)}>+</button></div>
         })
       }
+
+      {/* <button onClick={handleClick1}>
+        Team 1
+      </button>
+      <button onClick={handleClick2}>
+        Team 2
+      </button>
+      <button onClick={handleClick3}>
+        Team 3
+      </button> */}
     </div>
   )
 }
