094880cb06397bab277bb93aeccc634fbc2e7676
Dikshant Rajput <dikshantraj2001@gamil.com>
2022-02-11 13:43:59 +0530

    add

--- src/Gunrtc.js
+++ src/Gunrtc.js
@@ -1,32 +1,35 @@
 import React, {useState,useEffect,useRef} from 'react'
 import Gun from 'gun'
+require('gun/lib/load.js')
 
 function Gunrtc() {
   const [name,setName] = useState('')
+  const [points,setPoints] = useState(0)
   const [users,setUsers] = useState(null)
   const [localUsers,setLocalUsers] = useState([])
 
   const gun = useRef(null)
   
   useEffect(()=>{
-    gun.current = Gun('https://test-appsasdas.herokuapp.com/gun')
+    gun.current = Gun('http://localhost:3001/gun')
     setUsers(gun.current.get('users'))
-    gun.current.get('users').map().once((item,id)=>{
-      setLocalUsers((prev) => [...prev,{name : item?.n, id : id}])
-    })
+    gun.current.get('users').get('teams').map().once((item,id)=>{
+      setLocalUsers((prev)=>[...prev,{id,name:item?.n,points:item?.p}])
+    },{wait:0})
   },[])
 
   const clickHandler = ()=>{
-    users.set({n:name})
+    users.get('teams').set({n:name,p:points})
   }
-
+  
   return (
     <div>
       <input value={name} onChange={(e)=>setName(e.target.value)} />
+      <input value={points} onChange={(e)=>setPoints(e.target.value)} />
       <button onClick={clickHandler} >Add</button>
       {
-        localUsers.map((i)=>{
-          return <div key={i?.id}>{i?.name}</div>
+        localUsers.sort((a,b)=>a.points - b.points).map((i)=>{
+          return <div key={i?.id}>{i?.name} - {i?.points} </div>
         })
       }
     </div>
